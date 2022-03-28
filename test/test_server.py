e5bd0940426a58a2fc286acf42151a2e2ce93fc2
Emma Sovik <emmasovik@dhcp-10-22-64-237.wlan.ntnu.no>
2022-03-28 11:03:26 +0200

    Test

---
+++ test/test_server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+import socket
+from .test_videofeed import VideoFeed
+from .test_videosocket import videosocket
+
+import time
+
+class Server:
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(("", 6000))
+        self.server_socket.listen(5)
+        self.videofeed = VideoFeed(1,"server",1)
+        print("TCPServer Waiting for client on port 5000")
+
+
+    def start(self):
+        while 1:
+            client_socket, address = self.server_socket.accept()
+            print("I got a connection from ", address)
+            vsock = videosocket(client_socket)
+            while True:
+
+                frame=vsock.vreceive()
+                self.videofeed.set_frame(frame)
+                frame=self.videofeed.get_frame()
+                vsock.vsend(frame)
+#print frame
+#                self.videofeed.set_frame(frame)
+                 
+#data = client_socket.recv(921600)
+#                print "RECIEVED:" , data
+
+if __name__ == "__main__":
+    server = Server()
+    server.start()
