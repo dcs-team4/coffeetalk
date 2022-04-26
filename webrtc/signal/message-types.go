287034454a0637f4fd60f359ef87c06b993f2211
hermannm <hermann.morkrid@gmail.com>
2022-04-26 10:24:39 +0200

    add signal package with message types for webrtc signaling

---
+++ webrtc/signal/message-types.go
@@ -0,0 +1,62 @@
+package signal
+
+// Message types allowed to and from this server.
+const (
+	MsgVideoOffer   string = "video-offer"
+	MsgVideoAnswer  string = "video-answer"
+	MsgICECandidate string = "new-ice-candidate"
+	MsgJoinStream   string = "join-stream"
+	MsgLeaveStream  string = "leave-stream"
+	MsgError        string = "error"
+)
+
+// Base struct to embed in all message types.
+type BaseMessage struct {
+	// The kind of message that is sent.
+	// Messages that share the same type should always have the same structure.
+	Type string `json:"type"`
+}
+
+// Utility struct to embed in messages are to be sent directly from one user to another.
+type TargetedMessage struct {
+	// Username of sender.
+	From string `json:"from"`
+
+	// Username of destination.
+	To string `json:"to"`
+}
+
+// Message sent between two clients when initiating a stream between each other.
+type SDPExchangeMessage struct {
+	BaseMessage     // Type: MsgVideoOffer/MsgVideoAnswer.
+	TargetedMessage // Sender and receiver of video offer/answer.
+
+	// Session Description Protocol string: Describes a client's video stream config in order to
+	// properly set up a peer-to-peer stream.
+	SDP string `json:"sdp"`
+}
+
+// Message sent between two clients when initiating a stream between each other.
+type ICEExchangeMessage struct {
+	BaseMessage     // Type: MsgICECandidate.
+	TargetedMessage // Sender and receiver of ICE candidate.
+
+	// The ICE (Interactive Connectivity Establishment) protocol is used to negotiate the connection
+	// between two peers. This message allows clients to exchange ICE candidates between each other.
+	ICECandidate string `json:"iceCandidate"`
+}
+
+// Message sent from server to client when a received message causes an error.
+type ErrorMessage struct {
+	BaseMessage // Type: MsgError
+
+	ErrorMessage string `json:"errorMessage"`
+}
+
+// Utility function for constructing a new ErrorMessage.
+func NewErrorMessage(errMsg string) ErrorMessage {
+	return ErrorMessage{
+		BaseMessage:  BaseMessage{Type: MsgError},
+		ErrorMessage: errMsg,
+	}
+}
