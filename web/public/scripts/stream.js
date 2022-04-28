38f6f2c00affe87851778f9fde58a62e80559dd2
hermannm <hermann.morkrid@gmail.com>
2022-04-28 18:00:43 +0200

    remove old stream.js

--- web/public/scripts/stream.js
+++
@@ -1,8 +0,0 @@
-//@ts-check
-
-window.onload = () => {
-  const hangupButton = document.querySelector("#hangup-button");
-  hangupButton.addEventListener("click", hangupCall);
-};
-
-function hangupCall() {}
