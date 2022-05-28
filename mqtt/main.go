590f24c84f678aa9cb79cbfb63b8fb977a53282c
hermannm <hermann.morkrid@gmail.com>
2022-05-28 11:40:31 +0200

    refactor mqtt server main file for readability

--- mqtt/main.go
+++ mqtt/main.go
@@ -8,47 +8,72 @@ import (
 
 	"github.com/dcs-team4/coffeetalk/mqtt/broker"
 	"github.com/dcs-team4/coffeetalk/mqtt/quiz"
+	mqtt "github.com/mochi-co/mqtt/server"
 )
 
 func main() {
-	// Gets ports from environment variables.
-	socketPort := os.Getenv("SOCKET_PORT")
-	if socketPort == "" {
-		socketPort = "1882"
-	}
-	tcpPort := os.Getenv("TCP_PORT")
-	if tcpPort == "" {
-		tcpPort = "1883"
-	}
+	socketPort, tcpPort := getEnv()
 
-	// Sets up channel to keep running the server until a cancel signal is received.
+	// Sets up channel to keep running the server until it crashes, or a cancel signal is received.
 	close := make(chan struct{}, 1)
 	go handleCancelSignal(close)
 
-	// Runs MQTT broker concurrently.
-	mqttBroker := broker.New(socketPort, tcpPort)
-	go func() {
-		err := mqttBroker.Serve()
-		log.Println("MQTT broker failed:", err)
-		close <- struct{}{}
-	}()
-	log.Printf("MQTT broker listening on ports %v (WebSocket), %v (TCP)...\n", socketPort, tcpPort)
-
-	// Runs quiz state machine concurrently, and listens for quiz start messages on the broker.
-	quizmachine := quiz.NewMachine(mqttBroker)
-	go func() {
-		err := quizmachine.Run()
-		log.Println("Quiz state machine failed:", err)
-		close <- struct{}{}
-	}()
-	mqttBroker.Events.OnMessage = quizmachine.StartQuizHandler()
-	log.Println("Running quiz state machine...")
+	mqttBroker := runBroker(socketPort, tcpPort, close)
+	runQuizMachine(mqttBroker, close)
 
 	// Waits until server cancels/crashes.
 	<-close
 
 	mqttBroker.Close()
 	log.Println("Server closed.")
+}
+
+// Runs MQTT broker concurrently on the given ports, and returns it.
+// Sends on the given close channel if it crashes.
+func runBroker(socketPort string, tcpPort string, close chan<- struct{}) *mqtt.Server {
+	mqttBroker, err := broker.New(socketPort, tcpPort)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	go func() {
+		err := mqttBroker.Serve()
+		log.Println("MQTT broker failed:", err)
+		close <- struct{}{}
+	}()
+
+	log.Printf("MQTT broker listening on ports %v (WebSocket), %v (TCP)...\n", socketPort, tcpPort)
+	return mqttBroker
+}
+
+// Runs quiz state machine concurrently, and listens for quiz start messages on the given broker.
+// Sends on the given close channel if it crashes.
+func runQuizMachine(mqttBroker *mqtt.Server, close chan<- struct{}) {
+	quizmachine := quiz.NewMachine(mqttBroker)
+
+	go func() {
+		err := quizmachine.Run()
+		log.Println("Quiz state machine failed:", err)
+		close <- struct{}{}
+	}()
+
+	mqttBroker.Events.OnMessage = quizmachine.StartQuizHandler()
+	log.Println("Running quiz state machine...")
+}
+
+// Gets ports from environment variables.
+func getEnv() (socketPort string, tcpPort string) {
+	socketPort = os.Getenv("SOCKET_PORT")
+	if socketPort == "" {
+		socketPort = "1882"
+	}
+
+	tcpPort = os.Getenv("TCP_PORT")
+	if tcpPort == "" {
+		tcpPort = "1883"
+	}
+
+	return socketPort, tcpPort
 }
 
 // Sends on the given listener channel when a system cancel signal is received.
