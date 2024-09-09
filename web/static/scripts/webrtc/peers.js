8ffd42c8251e7ba3a1b087dcef8b7383750667cd
hermannm <hermann.morkrid@gmail.com>
2024-09-09 22:49:22 +0200

    Fix typo in STUN server address

--- web/static/scripts/webrtc/peers.js
+++ web/static/scripts/webrtc/peers.js
@@ -179,7 +179,7 @@ export function createPeerConnection(peerId, peerName) {
         urls: "stun:stun.l.google.com:19302",
       },
       {
-        urls: "stun:stun1.l.google.com:19302,",
+        urls: "stun:stun1.l.google.com:19302",
       },
       {
         urls: "stun:stun2.l.google.com:19302",
